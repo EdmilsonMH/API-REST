5ed4d7f2161006ceef112e802b571ab7e9d2d554
emerson <emersonmallquihuaman@gmail.com>
2024-09-01 09:57:34 -0700

    method create

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\post;
+use App\Models\Post;
 
 class PostController extends Controller
 {
@@ -13,80 +13,23 @@ class PostController extends Controller
      */
     public function index()
     {
-        return response()->json(Post::all(), 200);
+        $posts = Post::all();
+        return $posts;
+        //return response()->json(Post::all());
     }
 
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create(Request $request)
+    public function store(Request $request)
     {
-            // Validar los datos recibidos
+        // Validar los datos recibidos
         $validatedData = $request->validate([
             'title' => 'required|string|max:255',
             'content' => 'required|string',
         ]);
 
-        // Crear un nuevo registro con los datos validados
+        // Crear un nuevo registro en la base de datos
         $post = Post::create($validatedData);
 
-        // Retornar la respuesta JSON con el nuevo registro y el código de estado HTTP 201
+        // Retornar una respuesta JSON con el nuevo recurso y código de estado 201 (Creado)
         return response()->json($post, 201);
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        $post = Post::create($request->all());
-        return response()->json($post, 201);
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        $post = Post::find($id);
-        if ($post) {
-            return response()->json($post, 200);
-        }
-        return response()->json(['message' => 'Post not found'], 404);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        $post = Post::find($id);
-        if ($post) {
-            $post->update($request->all());
-            return response()->json($post, 200);
-        }
-        return response()->json(['message' => 'Post not found'], 404);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        $post = Post::find($id);
-        if ($post) {
-             $post->delete();
-            return response()->json(['message' => 'Post deleted'], 200);
-        }
-        return response()->json(['message' => 'Post not found'], 404);
     }
 }
